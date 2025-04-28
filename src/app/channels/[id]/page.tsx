dc1e24d1323c72f73fbbf127d0c94e03f2d6b53c
bannmoore <moore.brittanyann@gmail.com>
2025-04-27 18:53:07 -0700

    Update channel header UX.

--- src/app/channels/[id]/page.tsx
+++ src/app/channels/[id]/page.tsx
@@ -20,42 +20,38 @@ export default async function Page({
   const messages = await database.getRecentMessages(channel.id);
 
   return (
-    <>
-      <div className="mb-4">
-        <h1 className="mb-4">{channel.name}</h1>
-        <Link href={`/servers/${channel.serverId}`}>Back</Link>
+    <div className="text-left">
+      <div className="flex items-center justify-between my-4">
+        <div>
+          <h1>{channel.name}</h1>{" "}
+          <Link href={`/servers/${channel.serverId}`} className="link">
+            Back
+          </Link>
+        </div>
+        <span>
+          Last synced:{" "}
+          {channel.lastSyncedAt
+            ? channel.lastSyncedAt.toLocaleString()
+            : "Never"}
+          <div className="inline-block ml-4">
+            <SyncChannelButton channel={channel} />
+          </div>
+        </span>
       </div>
 
       <div className="my-4">
         <Alert />
       </div>
 
-      <div className="text-left">
-        <div className="flex justify-between my-4">
-          <h2>Recent Posts</h2>
-          <span>
-            Last synced:{" "}
-            {channel.lastSyncedAt
-              ? channel.lastSyncedAt.toLocaleString()
-              : "Never"}
-            <div className="inline-block ml-4">
-              <SyncChannelButton channel={channel} />
-            </div>
-          </span>
-        </div>
+      <div className="mt-4 text-left">
+        {messages.map((message) => {
+          if (message.isThread) {
+            return <ExpandableMessageCard key={message.id} message={message} />;
+          }
 
-        <div className="mt-4 text-left">
-          {messages.map((message) => {
-            if (message.isThread) {
-              return (
-                <ExpandableMessageCard key={message.id} message={message} />
-              );
-            }
-
-            return <MessageCard key={message.id} message={message} />;
-          })}
-        </div>
+          return <MessageCard key={message.id} message={message} />;
+        })}
       </div>
-    </>
+    </div>
   );
 }
